7037f0cd1fffe5774f81f509b049ece78fbd8019
Nikola <ristic1900@gmail.com>
2020-04-26 16:36:08 +0200

    first commit

---
+++ build/precache-manifest.b5a9e528c0d741beacdd78a13aaf9408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c45ec8d88bac9e6bfd492718faa2344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5e88e5141dec5fbb27b",
+    "url": "/static/css/main.c463a086.chunk.css"
+  },
+  {
+    "revision": "f8a87a0215f7ca3ebdc2",
+    "url": "/static/js/2.36edcc9b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.36edcc9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e88e5141dec5fbb27b",
+    "url": "/static/js/main.27c9c45f.chunk.js"
+  },
+  {
+    "revision": "601060052b73bcd6a0ac",
+    "url": "/static/js/runtime-main.da240beb.js"
+  }
+]);
